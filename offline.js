a4d26e11814e9ab9f1e5bdad034ebee975c0e4fe
guruchian <30399464+semicon@users.noreply.github.com>
2019-08-22 02:40:53 +0700

    currenttime
    currenttime

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1566415990,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/display-sheet0.png",
+		"images/sec-sheet0.png",
+		"images/mint-sheet0.png",
+		"images/hour-sheet0.png",
+		"images/bg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
